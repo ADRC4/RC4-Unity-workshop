ae6c15967846fb18c6e2259109ff6441f5d07ee6
visose <vicente@visose.com>
2018-10-22 17:31:24 +0100

    Third commit

---
+++ Assets/RotateCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        MoveMouse();
+    }
+
+    void MoveMouse()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            float mouseX = Input.GetAxis("Mouse X") * 2;
+            transform.RotateAround(Vector3.zero, Vector3.up, mouseX);
+        }
+    }
+}
--- Assets/Structure.cs
+++ Assets/Structure.cs
@@ -8,14 +8,43 @@ public class Structure : MonoBehaviour
 
     void Start()
     {
-        float gap = 1.02f;
+        Random.InitState(42);
+        CreateWall();
+    }
+
+    void CreateWall()
+    {
+        int layerWidth = 10;
+
+        for (int i = 0; i < 100; i++)
+        {
+            int layer = i / layerWidth;
+            int index = i % layerWidth;
+
+            Vector3 position = new Vector3(index, layer, 0) * 1.01f;
+
+            Quaternion rotation = Quaternion.identity;
+            Instantiate(Cube, position, rotation, transform);
+        }
+    }
+
+    void CreateColumn()
+    {
+        float gap = 1.01f;
 
         for (int i = 0; i < 10; i++)
         {
+            Vector3 position = Vector3.up * gap * i;
+            //var nudge = Random.insideUnitCircle * 0.3f;
+            //position.x += nudge.x;
+            //position.z += nudge.y;
+
+            Quaternion rotation = Quaternion.Euler(0, i * 8, 0);
+
             var goA = Instantiate(
                         Cube,
-                        Vector3.up * gap * i,
-                        Quaternion.identity,
+                        position,
+                        rotation,
                         this.transform
                         );
         }
