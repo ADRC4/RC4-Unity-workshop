9b40b1bb6814bcd9ed9245429ecf89b2c8608c13
Vicente Soler <vicente@visose.com>
2018-10-26 16:54:16 +0100

    Jenga

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
-    private void OnMouseDown()
+    void Start()
+    {
+        var renderer = this.GetComponent<MeshRenderer>();
+        var material = new Material(renderer.material);
+        //var color = Random.ColorHSV(0.5f, 0.7f, 0.5f, 0.8f, 0.5f, 0.5f);
+        var color = Color.white * Random.Range(0.2f, 0.6f);
+        material.color = color;
+        renderer.material = material;
+    }
+
+    void OnMouseDown()
     {
         var rb = this.GetComponent<Rigidbody>();
         var force = Vector3.up * 50;
--- Assets/Structure.cs
+++ Assets/Structure.cs
@@ -5,11 +5,38 @@ using UnityEngine;
 public class Structure : MonoBehaviour
 {
     public GameObject Cube;
+    Vector3 _size;
 
     void Start()
     {
         Random.InitState(42);
-        CreateWall();
+        Physics.autoSimulation = false;
+        // var collider = Cube.GetComponentInChildren<BoxCollider>();
+        //_size = collider.bounds.size;
+        _size = new Vector3(0.125f, 0.075f, 0.375f);
+        StartCoroutine(CreateJengaTower());
+    }
+
+    IEnumerator CreateJengaTower()
+    {
+        int layerWidth = 3;
+
+        for (int i = 0; i < 54; i++)
+        {
+            int layer = i / layerWidth;
+            int index = i % layerWidth;
+            float xPos = (index - 1) * _size.x;
+            float yPos = layer * _size.y;
+
+            Vector3 position = new Vector3(xPos, yPos, 0) * 1.01f;
+            Quaternion rotation = Quaternion.identity;
+            var block = Instantiate(Cube, position, rotation, transform);
+
+            if (layer % 2 == 0)
+                block.transform.RotateAround(Vector3.zero, Vector3.up, 90);
+
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     void CreateWall()
@@ -20,14 +47,15 @@ public class Structure : MonoBehaviour
         {
             int layer = i / layerWidth;
             int index = i % layerWidth;
-            float xPos = index - layerWidth / 2;
+            float xPos = (index - layerWidth / 2) * _size.x;
+            float yPos = layer * _size.y;
 
             if (layer % 2 == 0)
             {
-                xPos += 0.5f;
+                xPos += 0.5f * _size.x;
             }
 
-            Vector3 position = new Vector3(xPos, layer, 0) * 1.01f;
+            Vector3 position = new Vector3(xPos, yPos, 0) * 1.01f;
 
             Quaternion rotation = Quaternion.identity;
             Instantiate(Cube, position, rotation, transform);
@@ -53,6 +81,14 @@ public class Structure : MonoBehaviour
                         rotation,
                         this.transform
                         );
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 20, 100, 40), "Enable physics"))
+        {
+            Physics.autoSimulation = true;
         }
     }
 }
