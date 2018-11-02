4bf83dc893ac44aec2dd66376a771ba547fa4b7d
visose <vicente@visose.com>
2018-11-02 17:25:38 +0000

    Distance between collisions

--- Assets/JoinCube.cs
+++ Assets/JoinCube.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class JoinCube : MonoBehaviour
 {
+    public static float totalDistance = 0;
+
+
     private void OnCollisionEnter(Collision collision)
     {
+        var pa = this.transform.position;
+        var pb = collision.transform.position;
+        var delta = pa - pb;
+        var distance = delta.magnitude;
+        totalDistance += distance;
+
+
         if (collision.rigidbody == null)
             return;
 
