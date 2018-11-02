5c24eaafbf2a0f1b42d2471ac93dbb2dc10c6dc3
visose <vicente@visose.com>
2018-11-02 18:01:51 +0000

    Not change neighbours

--- Assets/SimpleCube.cs
+++ Assets/SimpleCube.cs
@@ -7,18 +7,41 @@ public class SimpleCube : MonoBehaviour
 {
     public int X, Y;
     public GameObject[,] Grid;
+    public bool Occupied = false;
 
     private void OnMouseDown()
     {
-        var renderer = this.GetComponent<MeshRenderer>();
-        var material = renderer.material;
-        material.color = Color.white * 0.8f;
+        if(!CheckNeighbours())
+        {
+            Occupied = true;
+            var renderer = this.GetComponent<MeshRenderer>();
+            var material = renderer.material;
+            material.color = Color.white * 0.8f;
+        }
 
-        if (X > 0) SetNeighbourColor(Grid[X - 1, Y]);
-        if (X < Grid.GetLength(0) - 1) SetNeighbourColor(Grid[X + 1, Y]);
+       //if (X > 0) SetNeighbourColor(Grid[X - 1, Y]);
+        //if (X < Grid.GetLength(0) - 1) SetNeighbourColor(Grid[X + 1, Y]);
 
-        if (Y > 0) SetNeighbourColor(Grid[X, Y - 1]);
-        if (Y < Grid.GetLength(1) - 1) SetNeighbourColor(Grid[X, Y + 1]);
+        //if (Y > 0) SetNeighbourColor(Grid[X, Y - 1]);
+        //if (Y < Grid.GetLength(1) - 1) SetNeighbourColor(Grid[X, Y + 1]);
+    }
+
+    bool CheckNeighbours()
+    {
+        var neighbours = new List<GameObject>();
+        if (X > 0) neighbours.Add(Grid[X - 1, Y]);
+        if (X < Grid.GetLength(0) - 1) neighbours.Add(Grid[X + 1, Y]);
+        if (Y > 0) neighbours.Add(Grid[X, Y - 1]);
+        if (Y < Grid.GetLength(1) - 1) neighbours.Add(Grid[X, Y + 1]);
+
+
+        foreach (var neighbour in neighbours)
+        {
+            if (neighbour.GetComponent<SimpleCube>().Occupied)
+                return true;
+        }
+
+        return false;
     }
 
     void SetNeighbourColor(GameObject neighbour)
