e9fcb605b0d4dcb87c99686f6cfa8ec7bc211292
visose <vicente@visose.com>
2018-11-02 16:58:03 +0000

    OOP More

--- Assets/OOPLearning.cs
+++ Assets/OOPLearning.cs
@@ -14,18 +14,72 @@ public class OOPLearning : MonoBehaviour
         animals.Add(new Cow());
         animals.Add(new Human());
         animals.Add(new Cat());
-       
-        foreach(var animal in animals)
-        Debug.Log(animal.MakeSound());
 
+        var dog = new Dog();
+        dog.Legs -= 1;
+        dog.Legs -= 1;
+        dog.Legs -= 1;
+        dog.Legs -= 1;
+        dog.Legs -= 1;
+
+        Debug.Log(dog.Legs);
+
+        //foreach (var animal in animals)
+        //    Debug.Log(animal.Sound);
+
+        var sum = MyMath.Add(10, 5);
+        var sine = Mathf.Sin(30);
+        var myDog = Animal.MakeDog();
+    }
+}
+
+static class MyMath
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
     }
 }
 
 
-class Animal
+abstract class Animal
 {
+
+   public static Animal MakeDog()
+    {
+        return new Dog();
+    }
+
     int _legs;
     string _sound;
+
+    public int Legs
+    {
+        get
+        {
+            return _legs;
+        }
+        set
+        {
+            if (value < 0)
+                _legs = 0;
+            else
+                _legs = value;
+        }
+    }
+
+    public string Sound
+    {
+        get
+        {
+            return _sound;
+        }
+        set
+        {
+            if (value == null) return;
+            _sound = value;
+        }
+    }
 
     public Animal(int legs, string sound)
     {
@@ -33,15 +87,15 @@ class Animal
         _sound = sound;
     }
 
-    public string MakeSound()
-    {
-        return _sound;
-    }
+    //public string MakeSound()
+    //{
+    //    return _sound;
+    //}
 }
 
 class Dog : Animal
 {
-    public Dog() : base(4,"Bark")
+    public Dog() : base(4, "Bark")
     {
 
     }
